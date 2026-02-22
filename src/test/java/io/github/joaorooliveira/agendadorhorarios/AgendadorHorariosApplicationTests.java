447fb75cf65964e6617da28fdcaff001b09145d3
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-02-22 14:13:17 -0300

    Initial commit

---
+++ src/test/java/io/github/joaorooliveira/agendadorhorarios/AgendadorHorariosApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.joaorooliveira.agendadorhorarios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgendadorHorariosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
