3adb616a2726c26aeb48cf804317f569ca75647c
Joaorooliveira <joaovictorooliveira812@gmail.com>
2026-02-22 14:24:28 -0300

    feat: cria entidade agendamento

---
+++ src/main/java/io/github/joaorooliveira/agendadorhorarios/infrastructure/entity/Agendamento.java
@@ -0,0 +1,35 @@
+package io.github.joaorooliveira.agendadorhorarios.infrastructure.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "agendamento")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Agendamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+    private String servico;
+
+    private String profissional;
+
+    private LocalDateTime dataHoraAgendamento;
+
+    private String cliente;
+
+    private String telefoneCliente;
+
+    private LocalDateTime dataInsercao = LocalDateTime.now();
+}
